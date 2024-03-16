ecfbf9aa171a5588c1dcf09180701ae07215eb64
vcadalso <vanessa.cadalso@gmail.com>
2024-03-16 14:56:24 -0400

    Set up html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Weather from "./Weather"
 export default function App() {
   return <div className="App">
     <div className="container">
-     <h1>Weather app</h1> 
+    
+     <Weather/>
   <footer>
     This project was coded by Vanessa Cadalso and is {" "}
-    <a href="https://github.com/vcadalso/react-week-five" target="_blank">Open-sourced </a>
+    <a href="https://github.com/vcadalso/react-week-five" target="_blank" rel="noreferrer">Open-sourced </a>
   </footer>
       </div>
     </div>
