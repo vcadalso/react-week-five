029a040e38461b04e2d6c3c1147104200b5f3dd2
vcadalso <vanessa.cadalso@gmail.com>
2024-03-16 14:11:02 -0400

    UPdated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
 import './App.css';
-
 export default function App() {
-  return <div className="App"> <h1>Weather app</h1> 
-  
-      
+  return <div className="App">
+    <div className="container">
+     <h1>Weather app</h1> 
+  <footer>
+    This project was coded by Vanessa Cadalso and is {" "}
+    <a href="https://github.com/vcadalso/react-week-five" target="_blank">Open-sourced </a>
+  </footer>
+      </div>
     </div>
-
 }
 
 
